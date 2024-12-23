14af4af4a605e1e6b11cc3f02d466baccfcae04a
milkymilky0116 <sjlee990129@gmail.com>
2024-12-23 09:18:58 +0900

    chore: update vote info

--- src/routes/introduce-vote/vote-guide/+page.svelte
+++ src/routes/introduce-vote/vote-guide/+page.svelte
@@ -1,4 +1,26 @@
 <!-- Title -->
 <h1 class="text-2xl font-bold mb-8 leading-tight">투표 방법</h1>
 
-<img src="https://github.com/user-attachments/assets/62190620-0d76-41bd-b72e-3bf7fdc01605" />
+<img
+	src="https://cdn.discordapp.com/attachments/1292303238518804521/1320537681473703946/51_.2025_...jpg?ex=6769f60d&is=6768a48d&hm=aec19899aad72da47e62bd36532bad772b76a675958bbfc02eb642d232b00956&"
+/>
+
+<img
+	src="https://cdn.discordapp.com/attachments/1292303238518804521/1320537681129639956/51_.2025_.._1.jpg?ex=6769f60d&is=6768a48d&hm=4c704a9fc03c49bc734d03681d2e1d88d5dd153927f0fc13addf514e1b13a2fa&"
+/>
+
+<img
+	src="https://cdn.discordapp.com/attachments/1292303238518804521/1320537680794222685/51_.2025_.._2.jpg?ex=6769f60d&is=6768a48d&hm=29655e385fda2409ad89d25719e4cd7d4165beff3201abb66a1f2ab5f0e165d3&"
+/>
+
+<img
+	src="https://cdn.discordapp.com/attachments/1292303238518804521/1320537680517271573/51_.2025_.._3.jpg?ex=6769f60d&is=6768a48d&hm=8ffb8528103d80a6c4c65d3b36da9024a371ca8bcaa1f598f14932085257b6bd&"
+/>
+
+<img
+	src="https://cdn.discordapp.com/attachments/1292303238518804521/1320537680228122678/51_.2025_.._5.jpg?ex=6769f60d&is=6768a48d&hm=c02cf0d58d3665bd8ff11639a719ba08be12d7d0b6d5fd48ae4186718cea4db1&"
+/>
+
+<img
+	src="https://cdn.discordapp.com/attachments/1292303238518804521/1320537679921680404/51_.2025_.._6.jpg?ex=6769f60d&is=6768a48d&hm=94e2bb88ad718e3e1ecf24b13d22eaafb02336c9b1ec63139c0ca9b3ecb61029&"
+/>
